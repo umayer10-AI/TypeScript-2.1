fd71d2b5cb8845e8a5191463d94dc0e91458972e
umayer10-AI <mdumayer10@gmail.com>
2026-05-23 15:08:05 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = 10;
+// let b:number = 20
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,2 @@
+let a:number = 10
+// let b:number = 20
