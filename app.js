7b63ecec66d83eaa8a627b8f8139d3b524521f80
umayer10-AI <mdumayer10@gmail.com>
2026-05-23 15:15:01 +0600

    add

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let a = '10';
+// let a = '10'
+let a;
+a = 10;
+a = true;
+a = "Umayer Ahamd";
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,1 +1,5 @@
-let a = '10'
+// let a = '10'
+let a: number | boolean | string;
+a = 10;
+a = true;
+a = "Umayer Ahamd"
